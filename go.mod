70f5508896dab721c9a194c1fcf7d6ddc53f1138
Andrew Snodgrass <pointlander@gmail.com>
2024-05-25 22:30:26 -0500

    Basic test

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/pointlander/kolmogorov
 
 go 1.19
 
-require github.com/pointlander/datum/iris v0.0.0-20200802052503-0ee610caba95 // indirect
+require (
+	github.com/pointlander/compress v1.1.1-0.20230129195249-46dfb34ef5b9 // indirect
+	github.com/pointlander/datum/iris v0.0.0-20200802052503-0ee610caba95 // indirect
+)
--- main.go
+++ main.go
@@ -5,6 +5,11 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"math"
+
+	"github.com/pointlander/compress"
 	"github.com/pointlander/datum/iris"
 )
 
@@ -13,5 +18,47 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	_ = datum
+	type Min struct {
+		Label string
+		Min   float64
+	}
+	mins := make([]Min, 150)
+	for i := range mins {
+		mins[i].Min = 1
+	}
+	vector := make([]byte, 8*64)
+	for i := range datum.Bezdek {
+		index := 0
+		for _, value := range datum.Bezdek[i].Measures {
+			bits := math.Float64bits(value)
+			for i := 0; i < 64; i++ {
+				vector[index] = byte(bits & 1)
+				bits >>= 1
+				index++
+			}
+		}
+		for j, entry := range datum.Bezdek {
+			if i == j {
+				continue
+			}
+			index := 4 * 64
+			for _, value := range entry.Measures {
+				bits := math.Float64bits(value)
+				for i := 0; i < 64; i++ {
+					vector[index] = byte(bits & 1)
+					bits >>= 1
+					index++
+				}
+			}
+			output := bytes.Buffer{}
+			compress.Mark1Compress1(vector, &output)
+			factor := float64(output.Len()) / float64(len(vector))
+			if factor < mins[i].Min {
+				mins[i].Min, mins[i].Label = factor, entry.Label
+			}
+		}
+	}
+	for i := range mins {
+		fmt.Println(mins[i].Label, mins[i].Min)
+	}
 }
